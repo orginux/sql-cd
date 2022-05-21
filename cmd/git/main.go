48aa191cd0798bc9648e8bdff0171af981119fc3
dgorobets <Denis.Gorobets@infobip.com>
2022-05-21 12:40:28 +0200

    use ssh key

--- cmd/git/main.go
+++ cmd/git/main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/orginux/sql-cd/cmd/logging"
 )
 
 // Clone clones a git project into a directory
@@ -14,11 +16,27 @@ func Clone(dir, gitUrl, branch string) error {
 	// Clean up
 	os.RemoveAll(dir)
 
-	_, err := git.PlainClone(dir, false, &git.CloneOptions{
+	privateKeyFile := "/tmp/key"
+	password := ""
+
+	_, err := os.Stat(privateKeyFile)
+	if err != nil {
+		logging.Warning.Printf("read file %s failed %s\n", privateKeyFile, err)
+	}
+
+	// Clone the given repository to the given directory
+	logging.Info.Printf("git clone %s ", gitUrl)
+	publicKeys, err := ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
+	if err != nil {
+		logging.Warning.Printf("generate publickeys failed: %s\n", err.Error())
+	}
+
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
 		URL:           gitUrl,
 		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
 		SingleBranch:  true,
 		Progress:      os.Stdout,
+		Auth:          publicKeys,
 	})
 	if err != nil {
 		return err
